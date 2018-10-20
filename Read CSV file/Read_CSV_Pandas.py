31fbc2d49c5f7a650d74f4222f83faefb3803d98
KamalGaider <kamal_gaider@yahoo.com>
2018-10-20 21:15:13 +0200

    read large csv using Pandas

---
+++ Read CSV file/Read_CSV_Pandas.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# CSV file used for below code contain - movieId :  all(customerId,Rating,Date)
+dataframe1 = pd.read_csv('combined_data_1.txt', header = None, names = ['Cust_Id', 'Rating'], usecols = [0,1])
+dataframe1['Rating'] = dataframe1['Rating'].astype(float)
+print('Dataset shape(row,column): {}\n'.format(dataframe1.shape))
+
+'''iloc is used to slice the dataframe- 
+Syntax--> datafm.iloc[row,column] --> returns rows containing index & data'''
+print('Dataset examples-\n')
+print(dataframe1.iloc[::1500000, :])
