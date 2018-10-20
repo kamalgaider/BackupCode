2ff1d8f0a0a270ebc8dfe67f341e3c3e21935563
KamalGaider <kamal_gaider@yahoo.com>
2018-10-20 21:18:55 +0200

    removed csv file

--- Read_CSV_Pandas.py
+++ Read_CSV_Pandas.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 # CSV file used for below code contain - movieId :  all(customerId,Rating,Date)
+#downloaded from -https://www.kaggle.com/laowingkin/netflix-movie-recommendation/data
 dataframe1 = pd.read_csv('combined_data_1.txt', header = None, names = ['Cust_Id', 'Rating'], usecols = [0,1])
 dataframe1['Rating'] = dataframe1['Rating'].astype(float)
 print('Dataset shape(row,column): {}\n'.format(dataframe1.shape))
