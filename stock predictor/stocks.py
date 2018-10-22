66458182b3f99f038465b16e12bf44b114eb1d3e
KamalGaider <kamal_gaider@yahoo.com>
2018-10-22 20:50:14 +0200

    bug fixed, added csv and generated image

--- stock predictor/stocks.py
+++ stock predictor/stocks.py
@@ -1,27 +1,32 @@
 import csv
 import numpy as np
-import matplotlib.pyplot as pyplot
+import matplotlib.pyplot as plt
 from sklearn.svm import SVR
 
 dates =[]
 prices=[]
 
-def get_data(filename, ):
+def get_data(filename):
 	with open(filename, 'r') as csvfile:
 		ireader = csv.reader(csvfile)
-		next(csvfile)
+		next(csvfile)# to remove headers from CSV file
 
+		#append dates & prices from csv to lists
 		for row in ireader:
 			dates.append(int(row[0].split('-')[0]))
 			prices.append(float(row[1]))
-	return
 
 def predict_prices(dates, prices, x) :
-	dates = np.reshape(dates, len(dates), 1)# make array using dates list, its length and its order
+	
+	# make array using (dates list, (length, its order))
+	dates = np.reshape(dates, (len(dates), 1))
+	
+	#Create models
 	svr_lin = SVR(kernel ='linear', C =1e3)
 	svr_poly = SVR(kernel ='poly', C =1e3, degree =2)
 	svr_rbf = SVR(kernel ='rbf', C =1e3, gamma =0.1)
 
+	#Train models
 	svr_lin.fit(dates,prices)
 	svr_poly.fit(dates,prices)
 	svr_rbf.fit(dates,prices)
@@ -33,7 +38,7 @@ def predict_prices(dates, prices, x) :
 	plt.plot(dates, svr_rbf.predict(dates), color='red', label = 'RBF model')
 
 	plt.xlabel('Date')
-	plt.xlabel('Price')
+	plt.ylabel('Price')
 	plt.title('Support Vector Regression')
 	plt.legend()
 	plt.show()
@@ -43,4 +48,3 @@ def predict_prices(dates, prices, x) :
 get_data('AAPL.csv')
 predicted_price= predict_prices(dates,prices,29)
 print(predict_prices)
-
